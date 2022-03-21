acd82eab54b6ebb10ebd65f72f3352bab02b43a8
Wei Bo Zheng <47736792+a076902768@users.noreply.github.com>
2022-03-21 18:57:32 +0800

    feat(header): 新增el-menu(樣式未調整)

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
   <ElConfigProvider :locale="locale">
-    <el-container style="height: 100%;">
-      <el-header class="header">縣市</el-header>
+    <el-container style="height: 100%">
+      <el-header class="header">
+        <el-menu
+          :default-active="activeIndex2"
+          class="el-menu-demo"
+          mode="horizontal"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+          @select="handleSelect"
+        >
+          <el-menu-item index="1">Processing Center</el-menu-item>
+          <el-menu-item index="3" disabled>Info</el-menu-item>
+          <el-menu-item index="4">Orders</el-menu-item>
+        </el-menu>
+      </el-header>
       <el-container class="container">
         <el-main>
           <router-view />
@@ -11,21 +25,28 @@
   </ElConfigProvider>
 </template>
 <script>
-import { defineComponent } from 'vue'
-import { ElConfigProvider } from 'element-plus'
-import lang from 'element-plus/lib/locale/lang/zh-tw'
+import { defineComponent, ref } from "vue";
+import { ElConfigProvider } from "element-plus";
+import lang from "element-plus/lib/locale/lang/zh-tw";
 
 export default defineComponent({
   components: {
     ElConfigProvider,
   },
   setup() {
+    const activeIndex = ref("1");
+    const activeIndex2 = ref("1");
+    const handleSelect = (key, keyPath) => {
+      console.log(key, keyPath);
+    };
     return {
       locale: lang,
-    }
+      activeIndex,
+      activeIndex2,
+      handleSelect
+    };
   },
-})
-
+});
 </script>
 
 <style lang="scss" scoped>
